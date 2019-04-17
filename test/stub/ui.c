d89b91ce1bb1eaba9c346fb2a07f42412f21ae66
Bianca van Schaik <bvschaik@gmail.com>
2019-04-17 21:15:31 +0200

    Add editor detection, load graphics

--- test/stub/ui.c
+++ test/stub/ui.c
@@ -15,6 +15,9 @@ void window_invalidate(void)
 void window_logo_show(void)
 {}
 
+void window_main_menu_show(void)
+{}
+
 void window_mission_end_show_fired(void)
 {}
 
@@ -22,6 +25,9 @@ void window_mission_end_show_won(void)
 {}
 
 void window_victory_dialog_show(void)
+{}
+
+void window_editor_map_show(void)
 {}
 
 window_id window_get_id(void)
